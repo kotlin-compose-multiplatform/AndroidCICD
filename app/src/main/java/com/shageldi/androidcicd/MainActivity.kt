9f3af31a4a8b67e3d95fc1eea1ad5ab4df5b70eb
Alysha <sh.alyyew2019@gmail.com>
2025-04-07 16:25:00 +0500

    Create UI testing

--- app/src/main/java/com/shageldi/androidcicd/MainActivity.kt
+++ app/src/main/java/com/shageldi/androidcicd/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shageldi.androidcicd.ui.theme.AndroidCICDTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +30,9 @@ class MainActivity : ComponentActivity() {
             AndroidCICDTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .consumeWindowInsets(innerPadding)
                     )
                 }
             }
@@ -31,17 +41,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    var counter by remember { mutableIntStateOf(0) }
+    Column(modifier) {
+        Button(modifier = Modifier.padding(16.dp), onClick = { counter++ })
+        {
+            Text(text = stringResource(R.string.increment_counter))
+        }
+        Text(text = stringResource(R.string.clicks, counter))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidCICDTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
