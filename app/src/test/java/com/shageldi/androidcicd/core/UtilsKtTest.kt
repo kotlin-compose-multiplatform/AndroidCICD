2cb389c8772b383f92431c58bd6c0248f3123fa4
Alysha <sh.alyyew2019@gmail.com>
2025-04-07 16:29:06 +0500

    UI Testing Example

--- app/src/test/java/com/shageldi/androidcicd/core/UtilsKtTest.kt
+++ app/src/test/java/com/shageldi/androidcicd/core/UtilsKtTest.kt
@@ -7,8 +7,8 @@ import org.junit.Test
 class UtilsKtTest {
     @Test
     fun `Test 2 numbers add function`() {
-        val expected = addTwoNumbers(2, 2)
-        val actual = 4
+        val expected = addTwoNumbers(4, 4)
+        val actual = 8
 
         assertThat(actual).isEqualTo(expected)
     }
